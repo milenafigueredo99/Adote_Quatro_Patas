37be64e2e71347484b609df8211b693217d39030
Milena Figueredo <milenafigueredo.s@gmail.com>
2022-04-15 14:32:46 -0300

    Adiciona estrutura incial do projeto

---
+++ src/test/java/com/br/drummond/quatropatas/AdoteQuatroPatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.drummond.quatropatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteQuatroPatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
