8ca67465960f04ae70ba5ce9a8391606ede036d8
Milena Figueredo <milenafigueredo.s@gmail.com>
2022-05-12 22:24:12 -0300

    bug fix

--- src/main/java/com/br/drummond/quatropatas/adapter/controller/PetController.java
+++ src/main/java/com/br/drummond/quatropatas/adapter/controller/PetController.java
@@ -7,20 +7,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Controller
+@CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/adote-quatropatas/pet")
 public class PetController {
 
     private final PetUseCase petUseCase;
     private final PetRegisterMapper petRegisterMapper;
+
+    @GetMapping
+    private ResponseEntity<?> findAllPets() {
+        var pets = petUseCase.getAllPets();
+        var test = petRegisterMapper.toResponse(pets);
+        return ResponseEntity.status(HttpStatus.OK).body(test);
+    }
 
     @PostMapping
     private ResponseEntity<?> petRegister(@Valid @RequestBody PetInput input) {
@@ -29,13 +34,9 @@ public class PetController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping
-    private ResponseEntity<?> findAllPets() {
-
-        var pets = petUseCase.getAllPets();
-        var test = petRegisterMapper.toResponse(pets);
-        return ResponseEntity.status(HttpStatus.OK).body(test);
+    @DeleteMapping
+    private ResponseEntity<?> petRegister(@RequestParam(name = "id") String externalId) {
+        petUseCase.deleteRegister(externalId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
-
-
 }
